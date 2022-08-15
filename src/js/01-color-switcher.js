c1aee7c97895d5a4c0b6cf70e6eb3dc644254f1c
Alex Dolhov <alexdolhov@gmail.com>
2022-08-16 00:37:30 +0300

    css comment

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,6 +2,7 @@ const refs = {
   startBtn: document.querySelector('button[data-start]'),
   stopBtn: document.querySelector('button[data-stop]'),
 };
+// console.log(refs.stopBtn);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
